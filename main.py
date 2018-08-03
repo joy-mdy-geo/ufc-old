98ff5085fb4f30c17ec725c6fda58fca893cdffe
Ba Nyar Naing <banyarnaing123@gmail.com>
2018-08-04 01:17:16 +0630

    convert the input data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify, request
+import controller
 
 app = Flask(__name__)
 
@@ -9,20 +10,14 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/_add_numbers')
-def add_numbers():
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
-
-
 @app.route('/_convert')
 def convert():
     from_text = request.args.get('from_text', '', type=str)
     to_text = request.args.get('to_text', '', type=str)
     from_encoding = request.args.get('from_encoding', '', type=str)
     to_encoding = request.args.get('to_encoding', '', type=str)
-    res = from_encoding + '\n' + to_encoding + '\n' + from_text
+    # res = from_encoding + '\n' + to_encoding + '\n' + from_text
+    res = controller.convert(from_encoding, to_encoding, from_text)
     return jsonify(result=res)
 
 
